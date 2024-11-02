cb056af5181e2f5de478cd693ff2ce40b4693fbe
Lucas Roberts <rlucas@Lucass-MacBook.local>
2024-11-02 16:35:50 -0400

    add sqlite-vec dll conn test at startup

--- vec_search/db.py
+++ vec_search/db.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import click
 from flask import current_app, g
 
+#HACK
+_SQLITE_VEC_DDL_PATH = "/Users/rlucas/sqlite-ext/sqlite-vec/dist/vec0.dylib"
 
 def get_db():
     if 'db' not in g:
@@ -12,6 +14,12 @@ def get_db():
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
+
+        g.db.enable_load_extension(True)
+        g.db.load_extension(_SQLITE_VEC_DDL_PATH)
+        g.db.enable_load_extension(False)
+        vec_version = g.db.execute("select vec_version()").fetchone()
+        print(f"vec_version={vec_version}")
 
     return g.db
 
