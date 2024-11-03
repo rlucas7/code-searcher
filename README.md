5e2eba28b884b644c8c07b66b2a803d24e4f9850
Lucas Roberts <rlucas@Lucass-MacBook.local>
2024-11-03 00:14:08 -0400

    add working impl of semantic vec search

--- README.md
+++ README.md
@@ -24,3 +24,21 @@ flask --app vec_search run --debug
 For the vectors we read these from a file at application startup time.
 This allows for the vectors to be embedded from somewhere else. e.g.
 any application-and any vector embedding method.
+
+## execute a semantic search
+
+You may have noticed the search form on the top of the listing page.
+If you enter a natural language description you'll see something like
+
+```bash
+"GET /?q=an+array+destructor+that+leaves+the+contents+intact HTTP/1.1" 200
+```
+
+in the stdout for the terminal where the flask app is running.
+Until now we did not have embedding for the natural language text,
+including the vector embeddings requires we install a few more ML dependencies.
+
+```bash
+pip install numpy==1.24 torch transformers
+```
+
--- vec_search/search.py
+++ vec_search/search.py
@@ -1,6 +1,9 @@
+import sqlite_vec
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
+from torch import tensor, reshape
+from transformers import RobertaTokenizer, RobertaForMaskedLM
 from werkzeug.exceptions import abort
 
 from vec_search.auth import login_required
@@ -9,18 +12,65 @@ from vec_search.db import get_db
 bp = Blueprint('search', __name__)
 
 
-@bp.route('/')
+_HF_MODEL = "microsoft/codebert-base-mlm"
+_MODEL = RobertaForMaskedLM.from_pretrained(_HF_MODEL)
+_TOKENIZER = RobertaTokenizer.from_pretrained(_HF_MODEL)
+
+
+# we hack the GET & disambiguate a search
+@bp.route('/', methods=["GET"])
 def index():
-    db = get_db()
-    # TODO: setup pagination
-    posts = db.execute(
-        'SELECT func_name, path, sha, code, doc FROM post limit 5'
-    ).fetchall()
-    print(len(posts))
-    for p in posts:
-        print(p)
-    print(len(posts))
-    return render_template('search/index.html', posts=posts)
+    print(f"request.args: {request.args}")
+    if request.method == "GET" and request.args.get("q") is None:
+        db = get_db()
+        # TODO: setup pagination
+        posts = db.execute(
+            'SELECT func_name, path, sha, code, doc FROM post limit 10'
+        ).fetchall()
+        print(len(posts))
+        return render_template('search/index.html', posts=posts)
+    elif request.method == "GET":
+        # load the LLM to embed the natural lang text to a vec
+        q = request.args.get('q')
+        print(q)
+        # block of code to prepare query
+        tokens = [_TOKENIZER.cls_token] + _TOKENIZER.tokenize(q) + [_TOKENIZER.eos_token]
+        raw_token_ids = tensor(_TOKENIZER.convert_tokens_to_ids(tokens))
+        tokens_ids = reshape(raw_token_ids, (1, len(raw_token_ids)))
+        context_embeddings = _MODEL(tokens_ids, output_hidden_states=True)
+        embeds = context_embeddings.hidden_states[-1].detach().numpy()[0, 0, :]
+        embed = embeds.tolist()
+        print(embed[0:10], len(embed))
+
+        # residual from copy paste
+        db = get_db()
+        cur = db.cursor()
+        vec_query = """
+            with knn_matches as (
+              select
+                rowid,
+                distance
+              from vec_items
+              where embedding match ?
+                and k = 5
+            )
+            select
+              func_name,
+              path,
+              sha,
+              code,
+              doc,
+              knn_matches.distance as distance
+            from knn_matches
+            left join post on post.id = knn_matches.rowid
+        """
+        cur.execute(vec_query, [sqlite_vec.serialize_float32(embed)])
+        posts = cur.fetchall()
+        print(f"length of embed-vectory search: {len(posts)}")
+        return render_template('search/index.html', posts=posts)
+
+    else:
+        raise ValueError("should not ever enter this branch")
 
 
 @bp.route('/create', methods=('GET', 'POST'))
