29fd8f36830d6307bfdc745f03fc5d516398f647
Lucas Roberts <rlucas@Lucass-MacBook.lan>
2025-01-06 13:56:04 -0500

    add >1 llm clients and llm rel gen
      * this adds:
        - support for gemini
        - add support for parsing output
        - add prompts
      * WIP/TODO:
        - abstract the interface for llm clients
        - add support for other llm clients

--- vec_search/RAD.sql
+++ vec_search/RAD.sql
@@ -5,11 +5,13 @@ q.user_id,
 GROUP_CONCAT(qr.relevance) AS relevances,
 qr.rank,
 qr.distance,
-q.query
-FROM query_relevances AS qr
+q.query,
+p.code,
+p.doc
+FROM query_relevances AS qr, post AS p
 INNER JOIN (
   SELECT query_id, query, user_id FROM queries
-) AS q ON qr.query_id = q.query_id
+) AS q ON qr.query_id = q.query_id AND p.id = qr.post_id
 GROUP BY 
 q.query_id,
 user_id,
