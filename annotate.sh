c809b319b5e7f92a76f4f0ba0035b4d9670efc29
Lucas Roberts <rlucas@Lucass-MacBook.lan>
2025-01-24 17:38:43 -0500

    cleanup whitepace I added mistakenly

--- annotate.sh
+++ annotate.sh
@@ -1,24 +1,24 @@
-#!/bin/bash                                                                     
-                                                                                
-echo "Enter your programming language, e.g. C, go, java, js, or python:"        
-read lang                                                                       
-echo "got: $lang"                                                               
-                                                                                
-echo "Enter your initials: "                                                    
-read initials                                                                   
-echo "got: $initials"  
+#!/bin/bash
+
+echo "Enter your programming language, e.g. C, go, java, js, or python:"
+read lang
+echo "got: $lang"
+echo "Enter your initials: "
+
+read initials
+echo "got: $initials"
 # could give them as cmd line args
 
-# openai key export  and google auth login can be included here or performed before start
-# modify config.db for lang first too
+# openai key export  and google auth login need to be  performed before start
+# modify config.py for lang first
 
 # follow terminal to interact with app
-# initdb and create new userid/pass for each lang and each user (every time we run annotate.sh)
+# initdb and for each lang and each user (every time we each run annotate.sh)
 
-flask --app vec_search init-db 
+flask --app vec_search init-db
 flask --app vec_search run --debug
 
-flask --app vec_search export-rad-to-csv rad-$lang-lang-$initials.csv 
+flask --app vec_search export-rad-to-csv rad-$lang-lang-$initials.csv
 
 # with openai
 flask --app vec_search gen-llm-rels rad-$lang-lang-$initials.csv llm_gen_rel-openai-$lang-$initials.csv
